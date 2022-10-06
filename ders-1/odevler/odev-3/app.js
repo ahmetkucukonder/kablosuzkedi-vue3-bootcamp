b5034ce770642a64981f29ae5260051ab0764524
Ahmet <kucukonderahmet2009@hotmail.com>
2022-10-06 06:31:06 +0300

    Update app.js

--- ders-1/odevler/odev-3/app.js
+++ ders-1/odevler/odev-3/app.js
@@ -1,6 +1,18 @@
-new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
+Vue.createApp({
+    data() {
+        return {
+            value: 0,
+            result: 'Henüz Bitmedi!!'
+        };
+    },
+    watch: {
+        value() {
+            this.result = this.value == 33 ? 'Tamamlandı' : 'Henüz Bitmedi!!';
+        },
+        result() {
+            setTimeout(() => {
+                this.value = 0;
+            }, 5000);
         }
-    });
+    }
+}).mount("#app");
